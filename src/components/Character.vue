9479daa26cc1600815c6363507c33166da0d2312
Alexis Moncada <alecabj12@gmail.com>
2021-04-13 05:24:22 -0500

    fetch api

---
+++ src/components/Character.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+          <img v-bind:src="character.image" v-bind:alt="character.name">
+          <div>
+            <h3>Name: {{character.name}} </h3>
+            <button>Ver mas</button>
+          </div>
+        </div>
+    </div>    
+</template>
+
+
+<script>
+export default {
+   props:['character']
+}
+</script>
---
+++ src/components/People.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <div>
+    <h1>STAR WARS CHARACTERS</h1>
+  </div>
+
+  <div>
+    <character v-for="character of characters" v-bind:key="character.id" v-bind:character="character" />
+  </div>
+     
+</div>
+</template>
+  
+
+<script>
+
+import axios from 'axios';
+import Character from '@/components/Character.vue';
+
+
+export default {
+  name: 'People',
+  components:{
+    Character
+  },
+
+   data: function(){
+    return {
+      characters:[],
+    }
+  },
+
+    created(){
+    this.fetch()
+  },
+
+
+
+  methods:{
+    fetch(){
+      let result =axios.get("https://swapi.dev/api/people/")
+      .then((res)=>{
+        this.characters = res.data.results
+        console.log(res.data)
+      })
+    }
+  }  
+}  
+</script>
+
